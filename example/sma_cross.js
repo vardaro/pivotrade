f727b3d2d2e4bd7f3fea6f5eaeb75726065d506d
vardaro <anthonyavardaro@yahoo.com>
2020-02-27 08:12:32 -0500

    add positions

--- example/sma_cross.js
+++ example/sma_cross.js
@@ -11,7 +11,26 @@ let session = new Session({
     }
 });
 
-session.backtest((payload) => {
-    console.log(payload);
+session.backtest((price, indicators) => {
+
+    let SMA20 = indicators.SMA20;
+    let SMA50 = indicators.SMA50;
+    
+    // If flat, and fast > slow, long 100% into holdings
+    if (session.holdings.length == 0) {
+        if (SMA20 < SMA50) {
+            let num_shares = Math.floor(session.capital / price.close);
+            let stop_loss = (.90) * price.close;
+            session.buy(price.close, num_shares, stop_loss, "gtc");
+        }
+    }
+
+    if (session.holdings.length == 1) {
+        let position = session.holdings[0];
+        if (SMA20 > SMA50 && position.limit < price.close) {
+            session.sell(0, price.close, session.holdings[0].quantity, "gtc");
+        }
+    }
+
 });
 
