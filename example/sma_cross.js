75ab264aa2f6900769bc34a1a1e1688cd29cbd84
vardaro <anthonyavardaro@yahoo.com>
2020-03-09 04:51:30 -0400

    minor typos

--- example/sma_cross.js
+++ example/sma_cross.js
@@ -6,8 +6,8 @@ const session = new Session({
   name: "SMA Crossover",
   symbol: "SPY",
   capital: 100000,
-  start_date: "2010-01-01",
-  end_date: "2020-01-01",
+  start_date: "2006-01-01",
+  end_date: "2010-01-01",
   indicators: {
     SMA50: new SMA(50),
     SMA20: new SMA(20)
@@ -20,18 +20,23 @@ session.backtest((price, account, indicators) => {
 
   // Price is provided as a Candlestick object
   let cur_price = price.close;
-  if (account.positions.length === 0) {
-    if (SMA20 > SMA50) {
-      let num_shares = Math.floor(account.capital / cur_price);
-      let stop_loss = 0.90 * cur_price;
-      session.buy({ limit: cur_price, quantity: num_shares, stop_loss: stop_loss});
-    }
+  if (SMA20 > SMA50 && account.positions.length === 0) {
+    let num_shares = Math.floor(account.capital / cur_price);
+    let stop_loss = 0.9 * cur_price;
+    session.buy({
+      limit: cur_price,
+      quantity: num_shares,
+      stop_loss: stop_loss
+    });
   }
 
-  if (account.positions.length === 1) {
+  if (SMA20 < SMA50 && account.positions.length === 1) {
     let position = account.positions[0];
-    if (SMA20 < SMA50) {
-      session.sell({ id: position.id, limit: cur_price, quantity: position.quantity });
-    }
+
+    session.sell({
+      id: position.id,
+      limit: cur_price,
+      quantity: position.quantity
+    });
   }
 });
